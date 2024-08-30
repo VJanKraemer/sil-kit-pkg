b902db6ce7ebf51339b538c5c69e93e1c5919464
Jan Kraemer <jan.kraemer@vector.com>
2024-08-30 07:58:58 +0200

    tests: add automatic tests to sil-kit-pkg
    Signed-off-by: Jan Kraemer <jan.kraemer@vector.com>

---
+++ tests/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include "silkit/SilKit.hpp"
+
+
+int main(void) {
+
+    try
+    {
+        auto participantConfig = SilKit::Config::ParticipantConfigurationFromString("{}");
+        auto participant = SilKit::CreateParticipant(participantConfig, "TestClient","silkit://localhost:8500");
+        return 0;
+    }
+    catch (const std::exception& error)
+    {
+        std::cerr << "Something went wrong: " << error.what() << std::endl;
+        std::cerr << "Exiting!" << std::endl;
+        return 1;
+    }
+}
